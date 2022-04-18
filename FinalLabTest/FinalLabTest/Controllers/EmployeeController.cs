5e0c1870f71f8dc6e5e9fe4558a755a43e9e9917
Aditya Yadav <adityayadavbhtr@gmail.com>
2022-04-18 18:54:42 +0530

    LogIn Authentication

---
+++ FinalLabTest/FinalLabTest/Controllers/EmployeeController.cs
@@ -0,0 +1,89 @@
+﻿using FinalLabTest.DB_Context;
+using FinalLabTest.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalLabTest.Controllers
+{
+    [Authorize]
+    public class EmployeeController : Controller
+    {
+        MytableContext dbobj = new MytableContext();
+        public IActionResult Index()
+        {
+            List<EmpRes> obj = new List<EmpRes>();
+
+            var res = dbobj.MyInfos.ToList();
+            foreach (var item in res)
+            {
+                obj.Add(new EmpRes
+                {
+                    Id = item.Id,
+                    Name = item.Name,
+                    Email = item.Email,
+                    Salary = item.Salary,
+                    City = item.City,
+                    Dept = item.Dept
+                });
+            }
+
+            return View(obj);
+        }
+        [HttpGet]
+        public IActionResult AddEmp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddEmp(EmpRes modobj)
+        {
+            MyInfo tblobj = new MyInfo();
+
+            tblobj.Id = modobj.Id;
+            tblobj.Name = modobj.Name;
+            tblobj.Email = modobj.Email;
+            tblobj.Salary = modobj.Salary;
+            tblobj.City = modobj.City;
+            tblobj.Dept = modobj.Dept;
+
+            if (modobj.Id == 0)
+            {
+                dbobj.MyInfos.Add(tblobj);
+                dbobj.SaveChanges();
+            }
+
+            else
+            {
+                dbobj.Entry(tblobj).State = EntityState.Modified;
+                dbobj.SaveChanges();
+            }
+
+            return RedirectToAction("Index", "Employee");
+        }
+        public IActionResult Edit(int id)
+        {
+            EmpRes modobj = new EmpRes();
+            var edit = dbobj.MyInfos.Where(a => a.Id == id).First();
+            modobj.Id = edit.Id;
+            modobj.Name = edit.Name;
+            modobj.Email = edit.Email;
+            modobj.Salary = edit.Salary;
+            modobj.City = edit.City;
+            modobj.Dept = edit.Dept;
+            return View("AddEmp", modobj);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var res = dbobj.MyInfos.Where(a => a.Id == id).First();
+            dbobj.MyInfos.Remove(res);
+            dbobj.SaveChanges();
+            return RedirectToAction("Index", "Employee");
+        }
+    }
+}
---
+++ FinalLabTest/FinalLabTest/Controllers/HomeController.cs
@@ -0,0 +1,139 @@
+﻿using FinalLabTest.DB_Context;
+using FinalLabTest.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace FinalLabTest.Controllers
+{
+
+    
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Authorize]
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult LogIn(UserReg objUser)
+        {
+            MytableContext dbobj = new MytableContext();
+
+            var res = dbobj.UserInfos.Where(a => a.Email == objUser.Email).FirstOrDefault();
+
+            if (res == null)
+            {
+
+                TempData["Invalid"] = "Email is not found";
+            }
+
+            else
+            {
+                if (res.Email == objUser.Email && res.Password == objUser.Password)
+                {
+
+                    var claims = new[] { new Claim(ClaimTypes.Name, res.Name),
+                                        new Claim(ClaimTypes.Email, res.Email) };
+
+                    var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                    var authProperties = new AuthenticationProperties
+                    {
+                        IsPersistent = true
+                    };
+                    HttpContext.SignInAsync(
+                    CookieAuthenticationDefaults.AuthenticationScheme,
+                    new ClaimsPrincipal(identity),
+                    authProperties);
+
+
+                    HttpContext.Session.SetString("Name", res.Name);
+
+                    return RedirectToAction("Index", "Employee");
+
+                }
+
+                else
+                {
+
+                    TempData["Wrong"] = "Wrong Password";
+
+                    return View("Login");
+                }
+
+
+            }
+
+
+            return View();
+        }
+        public IActionResult LogOut()
+        {
+            HttpContext.SignOutAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return RedirectToAction("LogIn", "Home");
+        }
+
+        [HttpGet]
+        public IActionResult NewUserReg()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult NewUserReg(UserReg sobj)
+        {
+            MytableContext dbobj = new MytableContext();
+            UserInfo tbl = new UserInfo();
+            tbl.Id = sobj.Id;
+            tbl.Name = sobj.Name;
+            tbl.Email = sobj.Email;
+            tbl.Password = sobj.Password;
+
+            dbobj.UserInfos.Add(tbl);
+            dbobj.SaveChanges();
+            return RedirectToAction("LogIn", "Home");
+        }
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
